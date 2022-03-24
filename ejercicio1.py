cb0dd14eb2d241fa8352b2a7868102b220b711f6
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-03-24 09:08:15 +0100

    funciones ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -3,5 +3,15 @@ class libro:
         self.autor = autor
         self.genero = genero
         self.isbn = isbn
-    def get_nombre(self,nombre):
-        self.nombre = nombre
+    
+    def get_autor(self):
+        return self.autor
+    
+    def get_genero(self):
+        return self.genero
+    
+    def get_isbn(self):
+        return self.isbn
+    
+final = libro ("jose","aventuras",92248)
+print (final.get_autor,final.get_genero,final.get_isbn)
